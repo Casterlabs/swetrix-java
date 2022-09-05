4235028d1369ef821815cb0c20c6ce1cd0f50e81
Ender <33337309+e3ndr@users.noreply.github.com>
2022-09-04 23:55:22 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# SwetrixApiJava
+
+```xml
+    <repositories>
+        <repository>
+            <id>jitpack.io</id>
+            <url>https://jitpack.io</url>
+        </repository>
+    </repositories>
+
+    <dependencies>
+        <dependency>
+            <groupId>com.github.casterlabs</groupId>
+            <artifactId>SwetrixApiJava</artifactId>
+            <version>1.0.0</version>
+            <scope>compile</scope>
+        </dependency>
+    </dependencies>
+```
---
+++ src/main/java/co/casterlabs/swetrixapijava/HttpUtil.java
@@ -0,0 +1,37 @@
+package co.casterlabs.swetrixapijava;
+
+import java.io.IOException;
+
+import org.jetbrains.annotations.Nullable;
+
+import co.casterlabs.rakurai.json.Rson;
+import co.casterlabs.rakurai.json.element.JsonObject;
+import lombok.NonNull;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+class HttpUtil {
+    private static final OkHttpClient client = new OkHttpClient();
+    private static final MediaType applicationJson = MediaType.parse("application/json");
+
+    static @Nullable JsonObject post(@NonNull String url, @NonNull JsonObject body) throws IOException {
+        try (Response response = client.newCall(
+            new Request.Builder()
+                .url(url)
+                .post(RequestBody.create(body.toString(), applicationJson))
+                .build()
+        ).execute()) {
+            String responseBody = response.body().string();
+
+            if (responseBody.isEmpty()) {
+                return null;
+            }
+
+            return Rson.DEFAULT.fromJson(responseBody, JsonObject.class);
+        }
+    }
+
+}
